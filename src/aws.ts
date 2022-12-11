8442a18ac3e82036d01b615b69946d008a59ab9c
Victor França Lopes <18534956+vflopes@users.noreply.github.com>
2022-12-11 18:57:03 -0300

    Refactor AWS SSM driver, and action

---
+++ src/aws.ts
@@ -0,0 +1,67 @@
+import AWS from 'aws-sdk'
+import { GetParametersByPathRequest, GetParametersByPathResult } from 'aws-sdk/clients/ssm'
+import core from '@actions/core'
+
+export const configure = () => {
+
+    let region = process.env.AWS_REGION || false
+
+    if (region) {
+        AWS.config.update({ region })
+    }
+
+}
+
+export const getFromSSMParameters = async (basePath: string, decryption: boolean): Promise<Map<string, string>> => {
+
+    const ssm = new AWS.SSM()
+
+    const params: GetParametersByPathRequest = {
+        Path: basePath,
+        Recursive: true,
+        WithDecryption: decryption
+    }
+
+    const kv = new Map<string, string>()
+
+    do {
+
+        await new Promise((resolve, reject) => {
+            ssm.getParametersByPath(params, (err: AWS.AWSError, data: GetParametersByPathResult) => {
+
+                if (err) {
+                    reject(err)
+                    return
+                }
+
+                if (!data.Parameters || data.Parameters.length == 0) {
+                    delete params.NextToken
+                    resolve(null)
+                    return
+                }
+
+                for (let parameter of data.Parameters) {
+                    
+                    core.info(`Getting value from SSM Parameter: ${parameter.Name}`)
+
+                    if (parameter.Name && parameter.Value) {
+
+                        kv.set(
+                            parameter.Name.substring(parameter.Name.lastIndexOf('/') + 1),
+                            parameter.Value
+                        )
+
+                    }
+
+                }
+
+
+
+            })
+        })
+
+    } while (params.NextToken)
+
+    return kv
+
+}
---
+++ src/index.ts
@@ -0,0 +1,57 @@
+import core from '@actions/core'
+import * as aws from './aws'
+
+async function main() {
+
+    const driver = core.getInput('driver')
+    const path = core.getInput('path')
+    const prefix = core.getInput('prefix') || ''
+    const sensitive = core.getBooleanInput('sensitive')
+
+    core.info(`Injecting environment variables from "${path}" (driver: "${driver}")`)
+
+    const [provider] = driver.split('.', 1)
+
+    switch (provider) {
+        case 'aws':
+            aws.configure()
+            break;
+    
+        default:
+            throw new Error(`Invalid provider: ${provider}`)
+    }
+
+    let kv: Map<string, string> | null = null
+
+    switch (driver) {
+        case 'aws.ssm_parameter':
+            kv = await aws.getFromSSMParameters(path, process.env.AWS_SSM_DECRYPT === 'true')
+            break;
+    
+        default:
+            throw new Error(`Invalid driver: ${driver}`)
+    }
+
+    if (kv === null) {
+        core.warning(`No values found to be injected in the workflow execution`)
+        return
+
+    }
+
+    for (let [key, value] of kv.entries()) {
+
+        const envKey = `${prefix}${key}`
+
+        core.info(`Set "$${envKey}" environment variable`)
+
+        if (sensitive) {
+            core.setSecret(value)
+        }
+        
+        core.exportVariable(envKey, value)
+
+    }
+
+}
+
+main().catch(err => core.setFailed(err))
